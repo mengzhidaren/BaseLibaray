ec502193ca244f8811c1fa5fb99bebb599904ee7
yuyunlong <273518021@qq.com>
2020-08-04 14:42:00 +0800

    init

--- baseAdapter/src/main/java/com/meicet/adapter/adapter/BaseMultiModeAttachAdapter.kt
+++ baseAdapter/src/main/java/com/meicet/adapter/adapter/BaseMultiModeAttachAdapter.kt
@@ -4,13 +4,13 @@ package com.meicet.adapter.adapter
 //多类型适配  继承BaseMultiMode
 //这个适合监听 视频播放器 的生命周期
 open class BaseMultiModeAttachAdapter(list: MutableList<BaseMultiMode>? = null) :
-    BaseMultiModeAdapter(list) {
+        BaseMultiModeAdapter(list) {
     private val tag = "BaseMultiModeAdapter"
 
     override fun onViewAttachedToWindow(holder: BaseHolder) {
         super.onViewAttachedToWindow(holder)
         val position = holder.adapterPosition - headerLayoutCount
-        if (position >= 0) {
+        if (position >= 0 && data.isNotEmpty()) {
             val index = findIndexByPosition(position)
             data[index].onViewAttachedToWindow(holder, position)
         }
@@ -19,7 +19,7 @@ open class BaseMultiModeAttachAdapter(list: MutableList<BaseMultiMode>? = null) 
     override fun onViewDetachedFromWindow(holder: BaseHolder) {
         super.onViewDetachedFromWindow(holder)
         val position = holder.adapterPosition - headerLayoutCount
-        if (position >= 0) {
+        if (position >= 0 && data.isNotEmpty()) {
             val index = findIndexByPosition(position)
             data[index].onViewDetachedFromWindow(holder, position)
         }
--- baseAdapter/src/main/java/com/meicet/adapter/databinding/adapter/ImageAdapter.kt
+++ baseAdapter/src/main/java/com/meicet/adapter/databinding/adapter/ImageAdapter.kt
@@ -1,11 +1,24 @@
 package com.meicet.adapter.databinding.adapter
 
+import android.net.Uri
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-
+import java.io.File
 
 
 @BindingAdapter("level")
 fun setlevel(imageView: ImageView, level: Int) {
     imageView.setImageLevel(level)
+}
+
+
+@BindingAdapter("imageFile")
+fun setImageFile(imageView: ImageView, path: String?) {
+    if (path != null) {
+        val file = File(path)
+        if (file.exists()) {
+            imageView.setImageURI(Uri.fromFile(file))
+        }
+    }
+
 }
