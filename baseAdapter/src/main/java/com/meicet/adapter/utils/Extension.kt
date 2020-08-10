02f272bb14df78a595fdae14ca0b2e04107a10be
yuyunlong <>
2020-08-10 22:42:50 +0800

    multimode

---
+++ baseAdapter/src/main/java/com/meicet/adapter/utils/Extension.kt
@@ -0,0 +1,20 @@
+package com.meicet.adapter.utils
+
+fun String.phoneEncryption():String{
+    if(this.length==11){
+        return  "${substring(0, 3)}****${substring(7, 11)}"
+    }else{
+        return ""
+    }
+
+}
+
+fun Int.format2() = String.format("%02d", this)
+
+fun Float.format1() = String.format("%.1f", this)
+fun Float.format2() = String.format("%.2f", this)
+
+fun Double.format1() = String.format("%.1f", this)
+fun Double.format2() = String.format("%.2f", this)
+
+fun Double.percent() = String.format("%.1f", this) + " %"
